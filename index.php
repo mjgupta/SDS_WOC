6d9da3da2df04164cea7677089148542f07a928b
mritunjay gupta <mritunjaygupta4@gmail.com>
2018-01-07 19:39:20 +0530

    Made home page ,with styling

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Travel Companion By SDSLabs</title>
+</head>
+<body>
+<div class="logo_bar">
+<a href="www.sdslabs.co">
+	<img src="logo.png" class="logo" alt="SDSlogo">
+</a>
+</div>
+<div class="WOC">
+	|WOC
+</div>
+<div class="title">myTravel Companion</div>
+<div class="end_bar">
+	<a href="form.php">Offer Companionship</a>
+	<a href="acc.php">My Account</a>
+</div>
+<div class="main">
+	<h1>Find a Companion</h1>
+</div>
+<div class="search_bar">
+	<form>
+		<fieldset>
+			<legend>
+				<span class="text_over_bar">Journey Details:</span>
+			</legend>
+			<input class="search_bar" list="source" autofocus placeholder="From" name="source">
+		&#8644;
+		<input class="search_bar" list="destination" placeholder="To" name="destination">
+		<input type="date" name="dated" class="search_bar" placeholder="Departure Date">
+		<button type="button" class="search_bar"><span>Find myCompanion</span></button>
+	</fieldset>
+
+	</form>
+</div>
+<div class="footer">
+	<p class="address" align="left">
+		<b>Reach us at :</b><br>
+		SDSlabs,<br>
+		Hobbies Club,<br>
+		IIT Roorkee , Roorkee<br>
+		<img src="mail_logo.png" alt="Mail icon"><a href="contact@sdslabs.co">contact@sdslabs.co</a><br>
+		<img src="call_logo.png" alt="Phone icon">01332-246866<br></p>
+	</p>
+	<p class="right_bottom" align="right">
+		<a href="https://www.facebook.com/SDSLabs/"><img src="fb.png"></a>
+		<a href="https://github.com/sdslabs"><img src="github.png"></a>
+		<a href="https://twitter.com/sdslabs"><img src="twitter.png"></a>
+	</p></div>
+	<div class="bottom">&#169;SDSlabs 2018 </div>
+
+</body>
+<!--Credits and sources
+	1.LOGO - https://studio.tailorbrands.com/
+
+</html>
