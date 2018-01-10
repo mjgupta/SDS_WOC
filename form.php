1914da44fa1df51166448a8d8700b9d60136742a
mritunjay gupta <mritunjaygupta4@gmail.com>
2018-01-10 23:44:22 +0530

    Made form.php and corresponding changes in the stylesheet

---
+++ form.php
@@ -0,0 +1,64 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Travel Companion By SDSLabs| Offer Companionship</title>
+</head>
+<body>
+<div class="logo_bar">
+<a href="www.sdslabs.co">
+	<img src="logo.png" class="logo" alt="SDSlogo">
+</a>
+</div>
+<div class="WOC">
+	|WOC
+</div>
+<div class="title">myTravel Companion</div>
+<div class="end_bar">
+	<a href="index.php">Find a Companion</a>
+	<a href="acc.php">My Account</a>
+</div>
+<div class="main">
+	<h1>Offer Companionship:</h1></div>
+<div class="form_main"><form method="POST">
+	<fieldset><legend>Personal Details</legend>
+			Name:<input type="text" name="name" placeholder="Your full name" class="form_main"><br>
+			Enrollment number:<input type="number" name="enroll_no" placeholder="Enrollment No.  here" min="130000" max="180000" class="form_main"><br>
+			Phone number: <input type="number" name="phone_no" placeholder="Phone no. here" class="form_main"><br>
+			<input type="checkbox" name="consent" checked class="form_main"> Share my contact details</fieldset><br>
+			<fieldset><legend>Details of the Journey</legend>
+			From:<input list="source" autofocus placeholder="From" required class="form_main" id="source"><br>
+			Destination:<input type="list" name="destination" required class="form_main" placeholder="To"><br>
+			Date:<input type="date" name="dated" class="search_bar" placeholder="Date of Travel" class="form_main"><br></fieldset>
+			<fieldset><legend>Additional Details:</legend>
+			PNR Number :<input type="number" name="PNR" class="form_main"><br>
+			No. of people: <input type="number" name="no_people" class="form_main"><br>
+			Major Stoppages/Comments: <textarea>Comments here . . . . . .</textarea>
+			</fieldset>
+</div>
+<button type="button" class="form_main">Submit
+</button>
+</form>
+</div>
+<div class="footer">
+	<p class="address" align="left">
+		<b>Reach us at :</b><br>
+		SDSlabs,<br>
+		Hobbies Club,<br>
+		IIT Roorkee , Roorkee<br>
+		<img src="mail_logo.png" alt="Mail icon"><a href="contact@sdslabs.co">contact@sdslabs.co</a><br>
+		<img src="call_logo.png" alt="Phone icon">01332-246866<br></p>
+	</p>
+	<p class="right_bottom" align="right">
+		<a href="https://www.facebook.com/SDSLabs/"><img src="fb.png"></a>
+		<a href="https://github.com/sdslabs"><img src="github.png"></a>
+		<a href="https://twitter.com/sdslabs"><img src="twitter.png"></a>
+	</p></div>
+	<div class="bottom">&#169;SDSlabs 2018 </div>
+
+</body>
+<!--Credits and sources
+	1.LOGO - https://studio.tailorbrands.com/
+-->
+</html>
